97d60668001b58e711b63b06531414e6a7340c25
xingguo <xingguo0828@gmail.com>
2020-07-16 16:42:17 +0800

    测试 缓存区数据存储格式

--- src/concurrent/cache/sync_pool_test.go
+++ src/concurrent/cache/sync_pool_test.go
@@ -1,10 +1,8 @@
 package cache
 
 import (
-	"runtime"
 	"sync"
 	"testing"
-	"time"
 )
 
 // 对象缓存测试
@@ -23,14 +21,15 @@ func TestSyncPool(t *testing.T) {
 		// 可以看出如果缓存中没有数据,创建新的对象不会自动写入到缓存中
 		t.Log("缓存中没有数据,第一次获取", v)
 	}
-	// 往缓存中写入数据,当私有对象中不存在数据时,会将数据写入到私有对象区
-	for i := 0; i < 2; i++ {
+	// 往缓存中写入数据,当私有对象中不存在数据时,会将数据写入到私有对象区;
+	//而对于缓存区的数据结构为栈,规则为后进先出,因此可以看到从缓冲区获取数据的顺序和写入的顺序相反
+	for i := 0; i < 4; i++ {
 		cache.Put(i)
 	}
 	// gc,
-	runtime.GC()
-	time.Sleep(time.Second * 1)
-	for i := 0; i < 3; i++ {
+	//runtime.GC()
+	//time.Sleep(time.Second * 1)
+	for i := 0; i < 5; i++ {
 		t.Log(cache.Get().(int))
 	}
 }
