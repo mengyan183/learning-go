96d1ca4322615111a3dabdf738044d56ed19aaf2
xingguo <xingguo0828@gmail.com>
2020-07-23 20:36:31 +0800

    原生json解析 以及 easyjson解析对比

--- src/json/json_opt_test.go
+++ src/json/json_opt_test.go
@@ -29,3 +29,54 @@ func TestJson(t *testing.T) {
 	}
 	t.Log(string(s))
 }
+
+func TestFastJson(t *testing.T) {
+	e := new(Employee)
+	//err := easyjson.Unmarshal([]byte(jsonStr), e)
+	err := e.UnmarshalJSON([]byte(jsonStr))
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(*e)
+	//s, err := easyjson.Marshal(e)
+	s, err := e.MarshalJSON()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(string(s))
+}
+
+func Benchmark_MarshalEasyJSON(b *testing.B) {
+	b.StartTimer()
+	e := new(Employee)
+	for i := 0; i < b.N; i++ {
+		err := e.UnmarshalJSON([]byte(jsonStr))
+		if err != nil {
+			b.Fatal(err)
+		}
+		b.Log(*e)
+		s, err := e.MarshalJSON()
+		if err != nil {
+			b.Fatal(err)
+		}
+		b.Log(string(s))
+	}
+}
+
+func Benchmark_JSON(b *testing.B) {
+	b.StartTimer()
+	e := new(Employee)
+	for i := 0; i < b.N; i++ {
+		err := json.Unmarshal([]byte(jsonStr), e)
+		if err != nil {
+			b.Fatal(err)
+		}
+		b.Log(*e)
+		s, err := json.Marshal(e)
+		if err != nil {
+			b.Fatal(err)
+		}
+		b.Log(string(s))
+	}
+
+}
