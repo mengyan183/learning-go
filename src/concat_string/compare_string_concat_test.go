1d8ef7c665716a3ee2ee94174994bf676a6e97ff
xingguo <xingguo0828@gmail.com>
2020-07-27 14:37:51 +0800

    对比 struct /map/slice 等 非基础数据类型 存储基础数据类型 数据传递模式

--- src/concat_string/compare_string_concat_test.go
+++ src/concat_string/compare_string_concat_test.go
@@ -44,7 +44,7 @@ func BenchmarkBytesBuffer(b *testing.B) {
 	b.StopTimer()
 }
 
-func BenchmarkStringsBuilder(b *testing.B)  {
+func BenchmarkStringsBuilder(b *testing.B) {
 
 	b.StartTimer()
 	for i := 0; i < b.N; i++ {
@@ -55,4 +55,59 @@ func BenchmarkStringsBuilder(b *testing.B)  {
 		_ = builder.String()
 	}
 	b.StopTimer()
+}
+
+func TestMapBasicDataType(t *testing.T) {
+	m := map[string]string{"1": "1"}
+	editMap(m)
+	// change
+	t.Log(m["1"])
+	im := map[string]int{"1": 1}
+	editIntMap(im)
+	// 2
+	t.Log(im["1"])
+}
+func editIntMap(m map[string]int) {
+	if m == nil {
+		return
+	}
+	m["1"] = 2
+}
+func editMap(m map[string]string) {
+	if m == nil {
+		return
+	}
+	m["1"] = "change"
+}
+
+type T struct {
+	i int
+}
+
+// 对比值和引用传递
+func TestMapStructType(t *testing.T) {
+	m := map[string]T{"1": {1}}
+	editStructMap(m)
+	// 1
+	t.Log(m["1"].i)
+	refm := map[string]*T{"1": {1}}
+	editStructRefMap(refm)
+	// 2
+	t.Log(refm["1"].i)
+}
+
+func editStructRefMap(m map[string]*T) {
+	if m == nil {
+		return
+	}
+	v, _ := m["1"]
+	v.i = 2
+}
+
+func editStructMap(m map[string]T) {
+	if m == nil {
+		return
+	}
+	v, _ := m["1"]
+	v.i = 2
 }
